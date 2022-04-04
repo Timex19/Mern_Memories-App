ddbfff267dce85b7ad091d9b41ce0a6c1f72d5b3
Ojo Timilehin Joseph <Ojooluwatimilehinpeter@yahoo.com>
2022-04-04 12:50:00 +0100

    Uploading all files

---
+++ client/src/reducers/posts.js
@@ -0,0 +1,40 @@
+import { CREATE, DELETE, FETCH_POST, FETCH_ALL, UPDATE, FETCH_BY_SEARCH, LIKE, START_LOADING, COMMENT, END_LOADING } from "../constants/actionsTypes";
+
+export default (state = { isLoading: true, posts: [] }, action) => {
+    switch (action.type) {
+        case START_LOADING:
+            return { ...state, isLoading: true };
+        case END_LOADING:
+            return { ...state, isLoading: false };
+        case FETCH_ALL:
+            return { 
+                ...state,
+                posts: action.payload.data,
+                currentPage: action.payload.currentPage,
+                numberOfPages: action.payload.numberOfPages,
+            };
+        case FETCH_BY_SEARCH:
+            return { ...state, posts: action.payload };
+        case FETCH_POST:
+            return { ...state, post: action.payload };
+        case LIKE:
+            return { ...state, posts:  state.posts.map((post) => (post._id === action.payload._id ? action.payload : post)) };
+        case COMMENT:
+            return { 
+                ...state, 
+                posts: state.posts.map((post) => {
+                if(post._id === action.payload._id) return action.payload;
+                
+                return post;
+                }),
+            };
+        case CREATE:
+            return { ...state, posts:  [...state.posts, action.payload] };
+        case UPDATE:
+            return { ...state, posts:  state.posts.map((post) => post._id === action.payload._id ? action.payload : post) };
+        case DELETE:
+            return { ...state, posts:  state.posts.filter((post) => post._id !== action.payload) };
+        default:
+            return state;
+    }
+};
